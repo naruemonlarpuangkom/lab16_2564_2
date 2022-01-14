b9fcb13cc8b5dfdb8d8c1f7bce919d2ff6fa9969
naruemonlarpuangkom <narumon.02545@gmail.com>
2022-01-14 15:37:16 +0700

    lab16_4FN

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,19 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &a,int &b,int &c,int &d){
+
+int s[4] = {a,b,c,d},temp,j ;
+
+for(int i = 0;i < 4;i++){
+	j = rand()%4 ;
+	temp = s[i] ;
+	s[i] = s[j] ;
+	s[j] = temp ;
+}
+a = s[0] ;
+b = s[1] ;
+c = s[2] ;
+d = s[3] ;
+
+} 
